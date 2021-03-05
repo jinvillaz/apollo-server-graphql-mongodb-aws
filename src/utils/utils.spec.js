8aa22ac59229dbafa1ca4db687241c236d1c95ce
Jhonatan Villanueva <jhonatan.villanueva@jalasoft.com>
2021-03-05 15:00:15 -0400

    Add unit test

--- src/utils/utils.spec.js
+++ src/utils/utils.spec.js
@@ -1,5 +1,45 @@
 import Utils from './utils';
 
-test('test sum equality.', () => {
-  expect(Utils.sum(2, 3)).toBe(5);
+test('Get data without null, undefined, and empty string', () => {
+  const personA = {
+    id: '1',
+    value: 'carlos',
+    figure: undefined,
+    empty: '',
+    extra: null,
+    extra2: 'test',
+  };
+  const personB = {
+    id: '1',
+    value: 'carlos',
+    extra2: 'test',
+  };
+  const result1 = JSON.stringify(Utils.getValidData(personA));
+  const result2 = JSON.stringify(personB);
+  expect(result1).toBe(result2);
+});
+
+test('Compare equaltiy properties between two objects.', () => {
+  const personA = {
+    id: '1',
+    value: 'carlos',
+  };
+  const personB = {
+    id: '1',
+    value: 'carlos',
+  };
+  expect(Utils.compare(personA, personB)).toBe(true);
+});
+
+test('The object are not equals.', () => {
+  const personA = {
+    id: '1',
+    value: 'carlos',
+  };
+  const personB = {
+    id: '1',
+    value: 'carlos',
+    extra: 'extradata',
+  };
+  expect(Utils.compare(personA, personB)).toBe(false);
 });
